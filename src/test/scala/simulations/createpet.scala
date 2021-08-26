5aa164f4a28085a7c7f555f80a512f5b54b3da6f
heldermendes <heldermendes1985@gmail.com>
2021-08-26 01:57:46 -0300

    ädd GET

--- src/test/scala/simulations/createpet.scala
+++ src/test/scala/simulations/createpet.scala
@@ -6,24 +6,36 @@ import io.gatling.core.scenario.Simulation
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
-class createpet extends Simulation {
+class pets extends Simulation {
 
   // protocol
   val httpProtocol = http.baseUrl("https://petstore.swagger.io")
     .acceptHeader("application/json").header("Content-type","application/json" )
 
   // define request
-  def createpet() = {
-    exec(http("create a user")
+  def createPets() = {
+    exec(http("create a pet")
       .post("/v2/pet")
-      .body(StringBody(session => s"""{"id":90909090123,"category":{"id":10,"name":"Fabu"},"name":"Cachorro","photoUrls":["string"],"tags":[{"id":2021,"name":"Novo"}],"status":"available"}""")).asJson)
+      .body(RawFileBody("./src/test/resources/bodies/pet1.json")).asJson
+      .check(status is(200)))
+  }
+
+  def findPets() = {
+    exec(http("find pet")
+      .get("/v2/pet/100319852000000")
+      .check(status is(200))
+    )
   }
 
   // setup scenario
   val scn = scenario("Trying to create new Dog")
-    .exec(createpet())
+    .exec(createPets())
+
+    .pause(3)
+
+    .exec(findPets())
 
   // inject users
-  setUp(scn.inject(atOnceUsers(10))
-    .protocols(httpProtocol))
+  setUp(scn.inject(atOnceUsers(2))
+    .protocols(httpProtocol));
 }
